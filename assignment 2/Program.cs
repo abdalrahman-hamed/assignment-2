3463e8805cdf97c0ffe71460a989e931a0d7d490
abdka_wi5pcm7 <abdka_wi5pcm7@ABDALRAHMAN-HAM>
2026-02-16 15:32:32 +0300

    Add project files.

---
+++ assignment 2/Program.cs
@@ -0,0 +1,42 @@
+﻿string str = "successfully";
+int countS = 0;
+int countU = 0;
+int countC = 0;
+int countE = 0;
+int countF = 0;
+int countL = 0;
+int countY = 0;
+foreach (var item in str)
+{
+    switch (item)
+    {
+        case 's':
+            countS++;
+            break;
+        case 'u':
+            countU++;
+            break;
+        case 'c':
+            countC++;
+            break;
+        case 'e':
+            countE++;
+            break;
+        case 'f':
+            countF++;
+            break;
+        case 'l':
+            countL++;
+            break;
+        case 'y':
+            countY++;
+            break;
+    }
+}
+Console.WriteLine($"S = {countS}");
+Console.WriteLine($"U = {countU}");
+Console.WriteLine($"C = {countC}");
+Console.WriteLine($"E = {countE}");
+Console.WriteLine($"F = {countF}");
+Console.WriteLine($"L = {countL}");
+Console.WriteLine($"Y = {countY}");
