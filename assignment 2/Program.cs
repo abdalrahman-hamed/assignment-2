7935f4f8edfc8b67d68a062d588981c9933e15c1
abdalrahman hamed <abdkan2005@gmail.com>
2026-02-21 21:55:18 +0300

    Update Program.cs
    new assignment 2

--- assignment 2/Program.cs
+++ assignment 2/Program.cs
@@ -1,42 +1,21 @@
-﻿string str = "successfully";
-int countS = 0;
-int countU = 0;
-int countC = 0;
-int countE = 0;
-int countF = 0;
-int countL = 0;
-int countY = 0;
-foreach (var item in str)
+Console.Write($"Pleaase Enter the word : ");
+string str = Console.ReadLine();
+Dictionary <char,int> letterCounts = new Dictionary<char,int>();
+foreach (var item  in str)
 {
-    switch (item)
+    if (letterCounts.ContainsKey(item))
     {
-        case 's':
-            countS++;
-            break;
-        case 'u':
-            countU++;
-            break;
-        case 'c':
-            countC++;
-            break;
-        case 'e':
-            countE++;
-            break;
-        case 'f':
-            countF++;
-            break;
-        case 'l':
-            countL++;
-            break;
-        case 'y':
-            countY++;
-            break;
+        letterCounts[item]++; 
+    }
+    else
+    {
+        letterCounts.Add(item,1) ; 
     }
 }
-Console.WriteLine($"S = {countS}");
-Console.WriteLine($"U = {countU}");
-Console.WriteLine($"C = {countC}");
-Console.WriteLine($"E = {countE}");
-Console.WriteLine($"F = {countF}");
-Console.WriteLine($"L = {countL}");
-Console.WriteLine($"Y = {countY}");
+foreach(var item in letterCounts)
+{
+    if (item.Value > 1)
+    {
+        Console.WriteLine($"{item.Key} = {item.Value}");
+    }
+}
